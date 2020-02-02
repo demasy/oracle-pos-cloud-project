a1976e12e788fa2ba8c97b19e4a3b22041c5b065
Ahmed El-Demasy <domssa@live.com>
2020-02-02 22:44:30 +0300

    Data Model Updates

--- src/apps/adf/DLPOSSaaSApp/WebServicesClient/src/dl/oracle/apps/fusion/pos/test/Test.java
+++ src/apps/adf/DLPOSSaaSApp/WebServicesClient/src/dl/oracle/apps/fusion/pos/test/Test.java
@@ -1,13 +1,25 @@
 package dl.oracle.apps.fusion.pos.test;
 
-import roketto.oracle.apps.test.HelloWorld;
+import java.util.ArrayList;
+
+import roketto.oracle.apps.fusion.repo.common.client.pojo.Territoriy;
+import roketto.oracle.apps.fusion.repo.common.client.rest.Territories;
+
 public class Test {
  
     public static void main(String[] args) {
 
-        HelloWorld helloWorld = new HelloWorld();
-        String result = helloWorld.sayHello();
-      
+        Territories territories = new Territories();
+        // territories.getAllTerritoriesJSON();
+        ArrayList<Territoriy> territoriesArray = territories.getAllTerritories(300);
+        for (int i = 0; i < territoriesArray.size(); i++) {
+                Territoriy territoriy = new Territoriy();
+                territoriy = territoriesArray.get(i);
+                System.out.println("# " + i);
+                System.out.println("Code=> " + territoriy.getTerritoryCode());
+                System.out.println("Short Name=> " + territoriy.getTerritoryShortName());
+                System.out.println("");
+        }
         
     }
 }
